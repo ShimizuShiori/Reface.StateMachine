88a935426346815f63f00be0c9298860c698fc58
ShimizuShiori <116971263@qq.com>
2020-03-07 00:07:05 +0800

    创建 Events 命名空间，将事件相关类型放在其中

--- src/Reface.StateMachine/CodeBuilder/CodeStateMachine.cs
+++ src/Reface.StateMachine/CodeBuilder/CodeStateMachine.cs
@@ -1,8 +1,7 @@
-﻿using System;
+﻿using Reface.StateMachine.Events;
+using System;
 using System.Collections.Generic;
 using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Reface.StateMachine.CodeBuilder
 {
--- src/Reface.StateMachine/Events/StateMachinePushedEventArgs.cs
+++ src/Reface.StateMachine/Events/StateMachinePushedEventArgs.cs
@@ -1,10 +1,6 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
-namespace Reface.StateMachine
+namespace Reface.StateMachine.Events
 {
     public class StateMachinePushedEventArgs<TState, TAction> : EventArgs
     {
